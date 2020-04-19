07250be27017855544d8fd8550508cd9f286ea39
Nalin Das <nalindas9@gmail.com>
2020-04-19 17:47:54 -0400

    Solution - Needs some update

--- car.py
+++ car.py
@@ -14,11 +14,13 @@ print('Headers Loaded!')
 
 IMAGES_PATH = "/home/nalindas9/Documents/Courses/Spring_2020_Semester_2/ENPM673_Perception_for_Autonomous_Robots/Github/enpm673/template-tracker-lucas-kanade/Dataset/Car4/img"
 
-BOX_START = (63,49)
-BOX_END = (179,139)
-BOX_COLOR = (0,255,0)
-BOX_THICKNESS = 2
+
 def main():
+  BOX_START = (63,49)
+  BOX_END = (179,139)
+  BOX_COLOR = (0,255,0)
+  BOX_THICKNESS = 2
+  pprev = np.array([0,0,0,0,0,0])
   for frame in sorted(glob.glob(IMAGES_PATH + "/*")):
     print('Image:', frame.split("img/", 1)[1])
     img = cv2.imread(frame)
@@ -26,13 +28,14 @@ def main():
     img = np.array(img)
     if frame.split("img/", 1)[1] == '0001.jpg':
       template = img[BOX_START[1]:BOX_END[1], BOX_START[0]:BOX_END[0]]
-      template_frame = cv2.rectangle(img, BOX_START, BOX_END, BOX_COLOR, BOX_THICKNESS)
-      #cv2.imshow('Frame', template_frame)
-      #cv2.waitKey(0)
+    #template_frame = cv2.rectangle(img, BOX_START, BOX_END, BOX_COLOR, BOX_THICKNESS)
+    #cv2.imshow('Frame', template)
+    #cv2.waitKey(0)
     rect = [[BOX_START[0], BOX_START[1], 1], [BOX_END[0],BOX_START[1], 1], [BOX_START[0], BOX_END[1],1], [BOX_END[0], BOX_END[1], 1]]
     #cv2.imshow('Current frame', img)
     #cv2.waitKey(0)
-    lk_algo.affine_LK_tracker(img, template, rect, [0,0])   
+    BOX_START, BOX_END, p = lk_algo.affine_LK_tracker(img, template, rect, pprev) 
+    pprev = p  
     cv2.destroyAllWindows()
   
 if __name__ == '__main__':
--- lk_algo.py
+++ lk_algo.py
@@ -16,49 +16,89 @@ def jacobian(x, y):
   
 def affine_LK_tracker(img, tmp, rect, pprev):
   p1,p2,p3,p4 = np.transpose(np.array(rect[0])), np.transpose(np.array(rect[1])), np.transpose(np.array(rect[2])), np.transpose(np.array(rect[3]))
-  print('Template corners:', (p1,p2,p3,p4))
-  delta_p = [0,0,0,0,0,0]
-  p_matrix = np.array([[1+delta_p[0], delta_p[2], delta_p[4]],[delta_p[1], 1+delta_p[3], delta_p[5]], [0,0,1]])
-  print('P Matrix:', p_matrix)
-  # Step 1 - Getting new ROI in current frame by multiplying p_matrix with Template 
-  p1new, p2new, p3new, p4new = np.dot(p_matrix,p1), np.dot(p_matrix,p2), np.dot(p_matrix,p3), np.dot(p_matrix,p4)  
-  #cv2.imshow('template', tmp)
-  #cv2.waitKey(0) 
-  print('New ROI:', (p1new, p2new, p3new, p4new))
-  img_warp = img[p1new[1]:p4new[1], p1new[0]:p4new[0]]
-  #cv2.imshow('Warped Image', img_warp)
-  #cv2.waitKey(0)
-  # Step 2 - Compute the error Image: Template - Warped image
-  error_img = tmp - img_warp
-  #cv2.imshow('Error Image', error_img)
-  #cv2.waitKey(0)
-  # Step 3 - Compute the gradient of the current frame
-  x_grad, y_grad = np.gradient(img_warp)
-  gradient_map = [] 
-  #cv2.imshow('X gradient current frame', x_grad)
-  #cv2.waitKey(0)
-  #cv2.imshow('Y gradient current frame', y_grad)
-  #cv2.waitKey(0)
-  # Step 4 - Compute the Jacobian of the warp
-  jacob_func = np.vectorize(jacobian)
-  jacobian_map = []
-  steepest_descent = []
-  for i in range(img_warp.shape[0]):
-    for j in range(img_warp.shape[1]):
-      jacobian_map.append(jacobian(i, j))
-      gradient_map.append([x_grad[i,j], y_grad[i,j]]) 
-      steepest_descent.append(np.dot(np.array([x_grad[i,j], y_grad[i,j]]), np.array(jacobian(i, j))))  
-  jacobian_map = np.array(jacobian_map)
-  gradient_map = np.array(gradient_map)
-  print('Jacobian:', jacobian_map)
-  print('Gradient map:', gradient_map)
-  # Step 5 - Compute Steepest descent
-  steepest_descent = np.array(steepest_descent)
-  print('Steepest descent', steepest_descent)
-  # Step 6 - Compute the Hessian matrix
-  H = np.dot(np.transpose(steepest_descent), steepest_descent)
-  print('Hessian Matrix:', H, 'Shape:', H.shape)
-  # Step 7 - Compute updated delta P 
-  delta_p = multi_dot([np.linalg.inv(H), np.transpose(steepest_descent), error_img.flatten()])
-  print('Delta p:', delta_p)
+  #print('Template corners:', (p1,p2,p3,p4))
+  p = pprev
+  norm = 5
+  itr=0
   
+  for i in range(30):
+    print('iteration:', itr)
+    p_matrix = np.array([[1+p[0], p[2], p[4]],[p[1], 1+p[3], p[5]], [0,0,1]])
+    print('P Matrix:', p_matrix)
+    # Step 1 - Getting the new ROI in current frame and warping template to it
+    
+    """
+    img_warp = []
+    img_warp_coords = []
+    for i in range(tmp.shape[0]):
+      for j in range(tmp.shape[1]):
+        new_coords = np.dot(p_matrix, np.transpose(np.array([i,j,1])))[0:2]
+        img_warp.append(img[new_coords[0], new_coords[1]])
+        img_warp_coords.append(new_coords)
+    img_warp = np.array(img_warp)
+    img_warp = img_warp.flatten()
+    """
+    p1new, p2new, p3new, p4new = np.dot(p_matrix,p1)[0:2], np.dot(p_matrix,p2)[0:2], np.dot(p_matrix,p3)[0:2], np.dot(p_matrix,p4)[0:2] 
+    pts1 = np.float32([p1[0:2],p2[0:2],p3[0:2]])
+    pts2 = np.float32([p1new, p2new, p3new])
+    M = cv2.getAffineTransform(pts1,pts2)
+    rows,cols = img.shape
+    img_warp = cv2.warpAffine(img,M,(cols,rows))
+    #cv2.imshow('Warped image', img_warp)
+    #cv2.waitKey(0) 
+    img_warp = img_warp[int(p1new[1]):int(p4new[1]), int(p1new[0]):int(p4new[0])]
+    #cv2.imshow('template', tmp)
+    #cv2.waitKey(0) 
+    print('New ROI:', (p1new, p2new, p3new, p4new))
+    #input_pts = np.float32([[p1[0],p1[1]],[p2[0], p2[1]],[p3[0], p3[1]],[p4[0], p4[1]]])
+    #output_pts = np.float32([[p1new[0],p1new[1]],[p2new[0], p2new[1]],[p3new[0], p3new[1]],[p4new[0], p4new[1]]])
+    # Compute the perspective transform M
+    #M = cv2.getPerspectiveTransform(input_pts,output_pts)
+    #img_warp = img[int(p1new[1]):int(p4new[1]), int(p1new[0]):int(p4new[0])]
+    #img_warp = cv2.warpPerspective(tmp,M,(tmp.shape[1], tmp.shape[0]),flags=cv2.INTER_LINEAR)
+    #cv2.imshow('Warped Image', img_warp)
+    #cv2.waitKey(0)
+    img_warp = np.resize(img_warp, (tmp.shape))
+    # Step 2 - Compute the error Image: Template - Warped image
+    error_img = tmp - img_warp 
+    #error_img = np.reshape(error_img, (tmp.shape[0], tmp.shape[1]))
+    cv2.imshow('Error Image', error_img)
+    cv2.waitKey(0)
+    # Step 3 - Compute the gradient of the current frame
+    x_grad, y_grad = np.gradient(img_warp)
+    gradient_map = [] 
+    #cv2.imshow('X gradient current frame', x_grad)
+    #cv2.waitKey(0)
+    #cv2.imshow('Y gradient current frame', y_grad)
+    #cv2.waitKey(0)
+    # Step 4 - Compute the Jacobian of the warp
+    jacobian_map = []
+    steepest_descent = []
+    for i in range(img_warp.shape[0]):
+      for j in range(img_warp.shape[1]):
+        jacobian_map.append(jacobian(i, j))
+        gradient_map.append([x_grad[i,j], y_grad[i,j]]) 
+        steepest_descent.append(np.dot(np.array([x_grad[i,j], y_grad[i,j]]), np.array(jacobian(i, j))))  
+    jacobian_map = np.array(jacobian_map)
+    gradient_map = np.array(gradient_map)
+    print('Jacobian:', jacobian_map)
+    print('Gradient map:', gradient_map)
+    # Step 5 - Compute Steepest descent
+    steepest_descent = np.array(steepest_descent)
+    print('Steepest descent', steepest_descent)
+    # Step 6 - Compute the Hessian matrix
+    H = np.dot(np.transpose(steepest_descent), steepest_descent)
+    print('Hessian Matrix:', H, 'Shape:', H.shape)
+    # Step 7 - Compute updated delta P 
+    delta_p = multi_dot([np.linalg.inv(H), np.transpose(steepest_descent), error_img.flatten()])
+    print('Delta p:', delta_p)
+    # Step 8 - Compute Norm of delta P
+    norm = np.linalg.norm(delta_p)
+    print('Norm:', norm)
+    # Step 9 - Update P matrix
+    p = np.add(p, delta_p)
+    itr= itr+1
+  tracked_frame = cv2.rectangle(img, (int(p1new[0]), int(p1new[1])), (int(p4new[0]), int(p4new[1])), 255, 2)
+  cv2.imshow('tracked_frame', tracked_frame)
+  cv2.waitKey(0)   
+  return (int(p1new[0]), int(p1new[1])), (int(p4new[0]), int(p4new[1])), p
